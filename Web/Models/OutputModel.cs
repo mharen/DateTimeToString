7dd8a9acfe1e3832764699b215b4d5a763d98334
mharen <mharen@gmail.com>
2013-09-22 21:24:59 -0400

    cleaned up, a little, that collection initializer

--- Web/Models/OutputModel.cs
+++ Web/Models/OutputModel.cs
@@ -21,103 +21,36 @@ namespace Web.Models
                               Sublists =
                                   new List<Sublist>
                                       {
-                                          new Sublist
-                                              {
-                                                  Name = "dates",
-                                                  Pairs = new List<Pair>
-                                                              {
-                                                                  new Pair(when, "d"),
-                                                                  new Pair(when, "D"),
-                                                                  new Pair(when, "m"),
-                                                                  new Pair(when, "y")
-                                                              }
-                                              },
-                                          new Sublist
-                                              {
-                                                  Name = "times",
-                                                  Pairs = new List<Pair>
-                                                              {
-                                                                  new Pair(when, "t"),
-                                                                  new Pair(when, "T")
-                                                              }
-                                              },
-                                          new Sublist
-                                              {
-                                                  Name = "combos",
-                                                  Pairs = new List<Pair>
-                                                              {
-                                                                  new Pair(when, "f"),
-                                                                  new Pair(when, "F"),
-                                                                  new Pair(when, "g"),
-                                                                  new Pair(when, "G"),
-                                                                  new Pair(when, "o"),
-                                                                  new Pair(when, "R"),
-                                                                  new Pair(when, "s"),
-                                                                  new Pair(when, "u"),
-                                                                  new Pair(when, "U")
-                                                              }
-                                              }
+                                          new Sublist("dates", new Pair(when, "d"), new Pair(when, "D"),
+                                                      new Pair(when, "m"), new Pair(when, "y")),
+                                          new Sublist("times", new Pair(when, "t"), new Pair(when, "T")),
+                                          new Sublist("combos", new Pair(when, "f"), new Pair(when, "F"),
+                                                      new Pair(when, "g"), new Pair(when, "G"), new Pair(when, "o"),
+                                                      new Pair(when, "R"), new Pair(when, "s"), new Pair(when, "u"),
+                                                      new Pair(when, "U"))
                                       }
                           });
 
             Lists.Add(new List
-            {
-                Name = "custom dates bits",
-                Sublists = new List<Sublist> {
-                                    new Sublist{
-                                        Name = "year",
-                                        Pairs = new List<Pair>{
-                                            new Pair(when, "yyyy"),
-                                            new Pair(when, "yy")
-                                        }
-                                    },
-                                    new Sublist{
-                                        Name= "month",
-                                        Pairs = new List<Pair>{
-                                            new Pair(when, "MM"),
-                                            new Pair(when, "%M")
-                                        }
-                                    },
-                                    new Sublist{
-                                        Name= "day",
-                                        Pairs = new List<Pair>{
-                                            new Pair(when, "ddd"),
-                                            new Pair(when, "dd"),
-                                            new Pair(when, "%d")
-                                        }
-                                    }
-                                }
-            });
+                          {
+                              Name = "custom dates bits",
+                              Sublists = new List<Sublist>
+                                             {
+                                                 new Sublist("year", new Pair(when, "yyyy"), new Pair(when, "yy")),
+                                                 new Sublist("month", new Pair(when, "MM"), new Pair(when, "%M")),
+                                                 new Sublist("day", new Pair(when, "ddd"), new Pair(when, "dd"),
+                                                             new Pair(when, "%d"))
+                                             }
+                          });
+
             Lists.Add(new List
                           {
                               Name = "custom time bits",
                               Sublists = new List<Sublist>
                                              {
-                                                 new Sublist
-                                                     {
-                                                         Name = "hour",
-                                                         Pairs = new List<Pair>
-                                                                     {
-                                                                         new Pair(when, "HH"),
-                                                                         new Pair(when, "hh")
-                                                                     }
-                                                     },
-                                                 new Sublist
-                                                     {
-                                                         Name = "minute",
-                                                         Pairs = new List<Pair>
-                                                                     {
-                                                                         new Pair(when, "mm")
-                                                                     }
-                                                     },
-                                                 new Sublist
-                                                     {
-                                                         Name = "second",
-                                                         Pairs = new List<Pair>
-                                                                     {
-                                                                         new Pair(when, "ss")
-                                                                     }
-                                                     }
+                                                 new Sublist("hour", new Pair(when, "HH"), new Pair(when, "hh")),
+                                                 new Sublist("minute", new Pair(when, "mm")),
+                                                 new Sublist("second", new Pair(when, "ss"))
                                              }
                           });
         }
@@ -131,8 +64,14 @@ namespace Web.Models
 
     public class Sublist
     {
+        public Sublist(string name, params Pair[] pairs)
+        {
+            Name = name;
+            Pairs = pairs;
+        }
+
         public string Name { get; set; }
-        public List<Pair> Pairs { get; set; }
+        public Pair[] Pairs { get; set; }
     }
 
     public class Pair
